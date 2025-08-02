607461eb3a020257ac0a0c23925e7619e512578f
Ildevertdaisy <ildevertdaisy@gmail.com>
2025-08-02 12:31:00 +0200

    Adding short essay logic

--- app.py
+++ app.py
@@ -4,15 +4,18 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
+
 @app.route("/")
-def home():
-    return render_template("index.html")
+def get_short_essays_screen():
+    return render_template("short_essays.html")
 
 
 @app.route("/mcq")
 def get_mcq_screen():
     return render_template("mcq_3.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8004)
